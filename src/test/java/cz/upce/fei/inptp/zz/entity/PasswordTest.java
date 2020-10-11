891bc99c743ac485e0108423ba0d134f8cc75409
Roman Diviš <divisak@gmail.com>
2020-10-11 20:45:18 +0200

    Testing tests

---
+++ src/test/java/cz/upce/fei/inptp/zz/entity/PasswordTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.upce.fei.inptp.zz.entity;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Roman
+ */
+public class PasswordTest {
+    
+    public PasswordTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        Password ppwd = new Password(0, "pass");
+        
+        assertTrue(ppwd.getPassword().equals("pass"));
+    }
+    
+}
